c718ec5c0f12f6c9b64159adffc2c19007839ca6
Queue <elbertq2@gmail.com>
2020-10-26 18:12:01 -0500

    partial completion of assignment 2 - first push

--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -3,9 +3,9 @@ namespace TechJobsOO
 {
     public class CoreCompetency
     {
-        private int id;
+        public int id { get; set; }
         private static int nextId = 1;
-        private string value;
+        public string value { get; set; }
 
         // TODO: Change the fields to auto-implemented properties.
 
@@ -35,6 +35,5 @@ namespace TechJobsOO
         {
             return value;
         }
-    }
     }
 }
